5ae1ad7ceb98191c820aa5514c3be5717ed6966a
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-09-16 16:57:17 -0300

    chore: Configuração inicial do projeto RESTful API

---
+++ src/rotas.js
@@ -0,0 +1,12 @@
+// Cadastrar Usuário
+// Fazer Login
+// Detalhar Perfil do Usuário Logado
+// Editar Perfil do Usuário Logado
+// Listar categorias
+// Listar transações
+// Detalhar transação
+// Cadastrar transação
+// Editar transação
+// Remover transação
+// Obter extrato de transações
+// [Extra] Filtrar transações por categoria
